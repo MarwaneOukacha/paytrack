7697d336ea596e655d758cd2eaa4bc95c918ce35
Marwane OUKACHA <oukachamarwane0@gmail.com>
2026-09-25 12:12:08 +0000

    allow CORS preflight on gateway: permit OPTIONS before JWT auth

--- paytrack-gateway/src/main/java/com/paytrack/paytrack_gateway/config/SecurityConfig.java
+++ paytrack-gateway/src/main/java/com/paytrack/paytrack_gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.paytrack.paytrack_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -15,6 +16,7 @@ public class SecurityConfig {
     return http
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
         .authorizeExchange(ex -> ex
+            .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/webjars/**",
                           "/payment-service/swagger-ui/**",
                           "/payment-service/v3/api-docs/**", "/v3/api-docs/**").permitAll()
